f90835373c21a4ef766c89ff35d64017de4dd7cb
mxro <mxro@nowhere.com>
2012-12-11 14:07:57 +1300

    building implementation

--- src/main/java/io/nextweb/sss/internal/DefaulStatefulService.java
+++ src/main/java/io/nextweb/sss/internal/DefaulStatefulService.java
@@ -1,8 +1,15 @@
 package io.nextweb.sss.internal;
 
 import io.nextweb.Link;
+import io.nextweb.Node;
+import io.nextweb.Query;
 import io.nextweb.Session;
+import io.nextweb.fn.Closure;
+import io.nextweb.fn.ExceptionListener;
+import io.nextweb.fn.ExceptionResult;
 import io.nextweb.jre.Nextweb;
+import io.nextweb.operations.exceptions.UndefinedListener;
+import io.nextweb.operations.exceptions.UndefinedResult;
 import io.nextweb.sss.NextwebStateServiceConfiguration;
 import de.mxro.server.contexts.GetPropertyCallback;
 import de.mxro.server.contexts.LogCallback;
@@ -18,20 +25,68 @@ public class DefaulStatefulService implements StatefulContext {
 	@Override
 	public void log(final String path, final String title,
 			final String message, final LogCallback callback) {
-		
-		
-		pl[p]
+		final Query messagesNode = root.select("./" + path, "messages");
+
+		final Query appendSafe = messagesNode.appendSafe(title).appendSafe(
+				message);
+
+		appendSafe.catchExceptions(new ExceptionListener() {
+
+			@Override
+			public void onFailure(final ExceptionResult r) {
+				callback.onFailure(r.exception());
+			}
+		});
+
+		appendSafe.get(new Closure<Node>() {
+
+			@Override
+			public void apply(final Node o) {
+				callback.onLogged();
+			}
+		});
 	}
 
 	@Override
 	public void setProperty(final String path, final Object value,
 			final SetPropertyCallback callback) {
+		final Query setValueSafe = root.select("./" + path, value)
+				.setValueSafe(value);
 
+		setValueSafe.catchExceptions(new ExceptionListener() {
+
+			@Override
+			public void onFailure(final ExceptionResult r) {
+				callback.onFailure(r.exception());
+			}
+		});
+
+		setValueSafe.get(new Closure<Node>() {
+
+			@Override
+			public void apply(final Node o) {
+				callback.onPropertySet();
+			}
+		});
 	}
 
 	@Override
 	public void getProperty(final String path,
 			final GetPropertyCallback callback) {
+		final Query select = root.select("./" + path);
+		select.catchUndefined(new UndefinedListener() {
+
+			@Override
+			public void onUndefined(final UndefinedResult r) {
+
+			}
+		});
+	}
+
+	@Override
+	public void getProperty(final String path, final Object defaultValue,
+			final GetPropertyCallback callback) {
+		// TODO Auto-generated method stub
 
 	}
 
